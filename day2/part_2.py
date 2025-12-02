bb1069af5a479ae414665cd5565dff85267ce7cd
ob1-dev <chris+github@ob1.nz>
2025-12-02 11:48:01 -0500

    feat: with ai changes

--- day2/part_2.py
+++ day2/part_2.py
@@ -5,8 +5,7 @@ def main():
     count = 0
     for line in get_input():
         for item in line.split(","):
-            print(item)
-            range_start, range_end = map(lambda x: int(x), item.split("-"))
+            range_start, range_end = map(int, item.split("-"))
             count += evaluate_range(range_start, range_end + 1)
     print(count)
 
@@ -14,19 +13,26 @@ def main():
 def evaluate_range(start, end):
     counter = 0
     for value in range(start, end):
-        number_string = str(value)
-        number_length = len(number_string)
-        for letter_range in range(1, number_length):
-            items = split_string(number_string, letter_range)
-            if all([item == items[0] for item in items]):
-                print(value)
-                counter += value
-                break
+        if has_repeating_pattern(value):
+            counter += value
     return counter
 
 
-def split_string(s, length):
-    return [s[i : i + length] for i in range(0, len(s), length)]
+def has_repeating_pattern(value):
+    number_string = str(value)
+    number_length = len(number_string)
+
+    # Only check divisors of the string length
+    for pattern_length in range(1, number_length):
+        if number_length % pattern_length != 0:
+            continue
+
+        # Check if the pattern repeats
+        pattern = number_string[:pattern_length]
+        if pattern * (number_length // pattern_length) == number_string:
+            return True
+
+    return False
 
 
 def get_input():
